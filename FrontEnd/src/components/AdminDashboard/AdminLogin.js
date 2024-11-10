7dcbe40cf6d254e74bae73007448432b61695a50
rakeshdas07 <i.am.rakeshdas07@gmail.com>
2024-11-10 23:39:14 +0530

    .

--- FrontEnd/src/components/AdminDashboard/AdminLogin.js
+++ FrontEnd/src/components/AdminDashboard/AdminLogin.js
@@ -1,10 +1,63 @@
+import { notify } from '../../helper/notify';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../../context/authContext';
+//import { useAuthContext } from '../../context/authContext';
 import './AdminLogin.css';
 
 const AdminLogin = () => {
-  const [adminData, setAdminData] = useState({
+
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  //const { dispatch } = useAuthContext()
+  const navigator = useNavigate()
+  var json = ''
+
+
+  notify('You have successfully logged in!')
+
+  const handleLogin = async (e) => {
+  
+      e.preventDefault()
+  
+      const login = {email, password}
+      setLoading(true)
+      const response = await fetch('http://localhost:3030/api/users/admin-login', {
+  
+        method: 'POST',
+        body: JSON.stringify(login),
+        headers : {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      setLoading(false)
+      json = await response.json()
+      notify(json, 'You have successfully logged in!')
+
+      if(!response) {
+        try {
+          console.log(json)
+        } catch(e) {
+          throw new Error(e)
+        }
+      }
+
+      if(response.ok) {
+        try {
+          localStorage.setItem('user', JSON.stringify(json))
+          //dispatch({type: 'LOGIN', payload: json})
+          setEmail('')
+          setPassword('')
+          navigator('/admin-dashboard')
+        } catch(e) {
+          throw new Error(e)
+        }
+      }
+    }
+
+  /*const [adminData, setAdminData] = useState({
     email: '',
     password: '',
   });
@@ -40,35 +93,34 @@ const AdminLogin = () => {
     } else {
       setError('INVALID EMAIL OR PASSWORD !');
     }
-  };
+  };*/
 
   return (
     <div className="admin-login-container">
       <div className="admin-login-form">
         <h2>Admin Login</h2>
 
-        {success && <div className="success-message">{success}</div>}
-        {error && <div className="error-message">{error}</div>}
 
-        <form onSubmit={handleSubmit}>
+        <div>
           <input
             type="email"
             name="email"
             placeholder="Email"
-            value={adminData.email}
-            onChange={handleChange}
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             required
           />
           <input
             type="password"
             name="password"
             placeholder="Password"
-            value={adminData.password}
-            onChange={handleChange}
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            autoComplete='current-password'
             required
           />
-          <button type="submit">Login</button>
-        </form>
+          <button type="submit" onClick={handleLogin} disabled={loading}>{loading ?<>loading...</>:<>login</>}</button>
+        </div>
       </div>
     </div>
   );
